4096b1e4a7d5e55765df7e43f998653259ce8ee9
Richard <Richard@COMPUTADORA>
2026-07-01 00:49:01 -0500

    Agregar archivos de proyecto.

---
+++ practica_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace practica_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Llamado a las funciones usando la nueva clase "actividad_1"
+            actividad_1.MostrarCabecera();
+
+            Console.Write("Ingrese el costo del servicio técnico: S/. ");
+            double costoServicio = double.Parse(Console.ReadLine());
+
+            // 2. Paso por Valor
+            double costoConImpuesto = actividad_1.CalcularTotalConIGV(costoServicio);
+
+            Console.WriteLine($"\nPrecio Base Original: S/. {costoServicio}");
+            Console.WriteLine($"Precio con IGV (Calculado por Valor): S/. {costoConImpuesto}");
+
+            // 3. Paso por Referencia
+            double descuentoFidelidad = 15.0;
+            actividad_1.AplicarDescuentoEspecial(ref costoConImpuesto, descuentoFidelidad);
+
+            // Verificación del cambio por referencia
+            Console.WriteLine($"Precio Final a Pagar (Modificado por Referencia): S/. {costoConImpuesto}");
+
+            // 4. Cierre de programa
+            actividad_1.MostrarPie();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ practica_5/actividad_1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace practica_5
+{
+    public class actividad_1
+    {
+        // ---  Funciones de encabezado propias ---
+        public static void MostrarCabecera()
+        {
+            Console.WriteLine("========================================");
+            Console.WriteLine("    SISTEMA DE SOPORTE Y VENTAS TECH    ");
+            Console.WriteLine("========================================");
+        }
+
+        public static void MostrarPie()
+        {
+            Console.WriteLine("\n========================================");
+            Console.WriteLine("   Fin del Proceso - Gracias por confiar");
+            Console.WriteLine("========================================");
+        }
+
+        // --- Paso por Valor ---
+        public static double CalcularTotalConIGV(double precioBase)
+        {
+            double igv = precioBase * 0.18;
+            return precioBase + igv;
+        }
+
+        // --- Paso por Referencia ---
+        public static void AplicarDescuentoEspecial(ref double precioFinal, double descuento)
+        {
+            precioFinal = precioFinal - descuento;
+            Console.WriteLine($"\n[INFO] Se aplicó un descuento directo de: S/. {descuento}");
+        }
+    }
+}
